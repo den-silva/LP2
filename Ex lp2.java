992b2d8bc7eabe98e99b8ffab215ba9b845c8ef2
den-silva <39094014+den-silva@users.noreply.github.com>
2020-02-19 20:54:53 -0300

    Add files via upload

---
+++ Ex lp2.java
@@ -0,0 +1,17 @@
+public class CalculoMedia{
+
+public static void main (String [] args){
+
+    int n1 = 10;
+    int n2 = 20;
+    int n3 = 45;
+
+    int media = (n1+n2+n3)/3;
+
+    System.out.println("numero 1 com o valor "+num1);
+    System.out.println("numero 2 com o valor "+num2);
+    System.out.println("numero 3 com o valor "+num3);
+    System.out.println("A média é " +media);
+
+    }
+}
